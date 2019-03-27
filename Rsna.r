b5cb58b710bf4d601a3451cce1b75aa7bbcfef32
Justin <cogijl@gmail.com>
2019-03-27 09:33:39 +0100

    Begin basic setup for library

---
+++ Rsna.r
@@ -0,0 +1,22 @@
+# basic scripts for semantic network analysis
+
+
+# import librarys
+library(quanteda)
+library(igraph)
+
+
+# convert string to network 
+
+
+# convert dataframe column to network
+
+
+# output network as different formats
+
+
+
+# output summary of global parameters for a network
+
+
+# output summary of network statistics in dataframe as its own dataframe
